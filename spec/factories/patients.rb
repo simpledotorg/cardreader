93bf956523bf9ba80012477dbd0354ed207ef32f
Govind Krishna Joshi <govind@nilenso.com>
2018-09-25 21:42:53 +0530

    Fix factory bot warning about static attributes

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -4,30 +4,30 @@ FactoryBot.define do
 
     sequence(:treatment_number) { |n| "2018-%08i" % n }
     registered_on { 3.months.ago }
-    alternate_id_number ""
-    name "Amir Singh"
-    gender "Male"
-    age 65
-    house_number ""
-    street_number ""
-    area "Test Area"
-    village "Test Village"
-    district "Mansa"
-    pincode "456123"
-    phone "91234 56789"
-    alternate_phone ""
-    already_on_treatment false
-    prior_heart_attack false
-    heard_attack_in_last_3_years false
-    prior_stroke false
-    chronic_kidney_disease false
-    medication1_name "Ibuprofen"
-    medication1_dose "200mg"
-    medication2_name ""
-    medication2_dose ""
-    medication3_name ""
-    medication3_dose ""
-    medication4_name ""
-    medication4_dose ""
+    alternate_id_number { "" }
+    name { "Amir Singh" }
+    gender { "Male" }
+    age { 65 }
+    house_number { "" }
+    street_number { "" }
+    area { "Test Area" }
+    village { "Test Village" }
+    district { "Mansa" }
+    pincode { "456123" }
+    phone { "91234 56789" }
+    alternate_phone { "" }
+    already_on_treatment { false }
+    prior_heart_attack { false }
+    heard_attack_in_last_3_years { false }
+    prior_stroke { false }
+    chronic_kidney_disease { false }
+    medication1_name { "Ibuprofen" }
+    medication1_dose { "200mg" }
+    medication2_name { "" }
+    medication2_dose { "" }
+    medication3_name { "" }
+    medication3_dose { "" }
+    medication4_name { "" }
+    medication4_dose { "" }
   end
 end
