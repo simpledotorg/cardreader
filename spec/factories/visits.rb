05e8984997d7f4289be2890141e201677360a9a3
Govind Krishna Joshi <govind@nilenso.com>
2018-09-26 13:54:51 +0530

    Revert "Setup semaphore"
    This reverts commit c3fa71d601d3bbf5aba997d63a002ce8ab36ddb0.

--- spec/factories/visits.rb
+++ spec/factories/visits.rb
@@ -5,15 +5,15 @@ FactoryBot.define do
     systolic { rand(100.160) }
     diastolic { rand(60..120) }
     measured_on { Faker::Date.between(3.months.ago, Date.today) }
-    blood_sugar { "" }
-    amlodipine { "10mg" }
-    telmisartan { "40mg" }
-    enalpril { "" }
-    chlorthalidone { "" }
-    aspirin { "" }
-    statin { "" }
-    beta_blocker { "" }
-    referred_to_specialist { false }
+    blood_sugar ""
+    amlodipine "10mg"
+    telmisartan "40mg"
+    enalpril ""
+    chlorthalidone ""
+    aspirin ""
+    statin ""
+    beta_blocker ""
+    referred_to_specialist false
     next_visit_on { measured_on + 1.month }
   end
 end
