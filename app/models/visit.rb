a7bda9ab78a1a7ea2b64ae50433d4f0304fca48e
Tim Cheadle <tim@rationalmeans.com>
2018-09-27 21:26:30 -0400

    Allow blank next visit dates

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,5 +2,5 @@ class Visit < ApplicationRecord
   belongs_to :patient, inverse_of: :visits
 
   validates_date :measured_on
-  validates_date :next_visit_on
+  validates_date :next_visit_on, allow_blank: true
 end
