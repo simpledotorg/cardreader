647c0286ee6dd8d579128db438e845b120ba2886
Govind Krishna Joshi <govind@nilenso.com>
2018-10-06 12:15:05 +0530

    Fix failing tests

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -19,7 +19,7 @@ class VisitsController < ApplicationController
   end
 
   def create
-    @visit = @patient.visits.new(visit_params)
+    @visit = @patient.visits.new(visit_params.merge(facilities_id: @facility.id))
 
     respond_to do |format|
       if @visit.save
