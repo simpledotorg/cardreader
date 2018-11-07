4f581280887390b2d9c44a94c508bce5a2e85a24
Tim Cheadle <tim@rationalmeans.com>
2018-11-07 14:56:02 +0530

    Added roles for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:password)}
+    it { should validate_presence_of(:role) }
+
+    it { should define_enum_for(:role).with([:admin, :operator]) }
+  end
 end
