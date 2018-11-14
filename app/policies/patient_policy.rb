8fd58657194c8dc92a726043b96691dd1a3f81b3
Tim Cheadle <tim@rationalmeans.com>
2018-11-14 19:56:57 +0530

    Added patient policy and specs

---
+++ app/policies/patient_policy.rb
@@ -0,0 +1,29 @@
+class PatientPolicy < ApplicationPolicy
+  def index?
+    user.admin? || user.operator?
+  end
+
+  def show?
+    user.admin? || user.operator?
+  end
+
+  def create?
+    user.admin? || user.operator?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user.admin? || user.operator?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user.admin? || user.operator?
+  end
+end
