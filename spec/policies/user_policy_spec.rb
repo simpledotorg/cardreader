0075ad824cc2743d1ceb83712b513db1aab9adc8
Tim Cheadle <tim@rationalmeans.com>
2018-11-14 19:56:48 +0530

    Fixed typos

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.describe UserPolicy do
   subject { described_class }
 
-  let(:owner) { create(:user, :admin) }
+  let(:admin) { create(:user, :admin) }
   let(:operator) { create(:user, :operator) }
 
   permissions :index?, :show?, :new?, :create?, :update?, :edit?, :destroy? do
-    it "permits owners" do
-      expect(subject).to permit(owner, User)
+    it "permits admins" do
+      expect(subject).to permit(admin, User)
     end
 
     it "denies operators" do
