09a6082a43e4da10b8b584183b8b6947cab3d2bc
Tim Cheadle <tim@rationalmeans.com>
2018-11-19 15:00:08 -0500

    Move to helpers for cleaner visit info rendering

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -12,9 +12,17 @@ module PatientsHelper
 
   def visit_info(value)
     if value.present?
-      content_tag(:span, class: "visit-info") { value }
+      content_tag(:span, value, class: "visit-info")
     else
-      content_tag(:span, class: "visit-info visit-info-null") { value }
+      content_tag(:span, "none", class: "visit-info visit-info-null")
+    end
+  end
+
+  def visit_info_drug(name, dose)
+    if name.present?
+      content_tag(:span, "#{name}<br>#{dose}".html_safe, class: "visit-info")
+    else
+      content_tag(:span, "none", class: "visit-info visit-info-null")
     end
   end
 end
