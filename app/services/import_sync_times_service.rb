c8a01a79436b1f7f1c567e8dc89db2ab0a93b589
Divyansh Prakash <divyansh.prakash@yahoo.com>
2018-11-23 16:42:07 +0530

    ImportSyncTimesService defines attribute readers instead of using @

--- app/services/import_sync_times_service.rb
+++ app/services/import_sync_times_service.rb
@@ -1,4 +1,6 @@
 class ImportSyncTimesService
+  attr_reader :class_to_update, :uuid_field, :file_uri
+
   def initialize(class_to_update, uuid_field, file_uri)
     @class_to_update = class_to_update
     @uuid_field = uuid_field
@@ -6,15 +8,15 @@ class ImportSyncTimesService
   end
 
   def import
-    CSV.foreach(@file_uri) do |row|
+    CSV.foreach(file_uri) do |row|
       begin
         id = row.first
         synced_at = row.last.to_time
 
-        record = @class_to_update.where("#{@uuid_field} = '#{id}'").first
+        record = class_to_update.where("#{uuid_field} = '#{id}'").first
         record.update_columns(synced_at: synced_at, last_sync_errors: nil)
       rescue => e
-        puts "Error while updating #{@class_to_update} with id: #{id}: #{e}"
+        puts "Error while updating #{class_to_update}.#{uuid_field} #{id}: #{e.message}"
       end
     end
   end
