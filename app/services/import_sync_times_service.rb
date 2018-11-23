0a237ff73c03bcf233ea39ebdea94b3b9f0d4b2f
Divyansh Prakash <divyansh.prakash@yahoo.com>
2018-11-23 15:17:21 +0530

    Add task for importing sync times from csv

---
+++ app/services/import_sync_times_service.rb
@@ -0,0 +1,21 @@
+class ImportSyncTimesService
+  def initialize(class_to_update, uuid_field, file_uri)
+    @class_to_update = class_to_update
+    @uuid_field = uuid_field
+    @file_uri = file_uri
+  end
+
+  def import
+    CSV.foreach(@file_uri) do |row|
+      begin
+        id = row.first
+        synced_at = row.last.to_time
+
+        record = @class_to_update.where("#{@uuid_field} = '#{id}'").first
+        record.update_columns(synced_at: synced_at, last_sync_errors: nil)
+      rescue => e
+        puts "Error while updating #{@class_to_update} with id: #{id}: #{e}"
+      end
+    end
+  end
+end
