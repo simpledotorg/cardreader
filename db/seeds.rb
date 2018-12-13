a0c8e9e19a72808bc1db144efe5794d0ecc4a241
Tim Cheadle <tim@rationalmeans.com>
2018-12-12 22:15:50 -0500

    Only run seeds in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,60 +6,62 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-District.destroy_all
-Facility.destroy_all
-Patient.destroy_all
-Visit.destroy_all
-User.destroy_all
+if Rails.env.development?
+  District.destroy_all
+  Facility.destroy_all
+  Patient.destroy_all
+  Visit.destroy_all
+  User.destroy_all
 
-admin = User.create(
-  email: "admin@example.com",
-  password: "password"
-)
-
-%w(
-  Bathinda
-  Gurdaspur
-  Hoshiarpur
-  Mansa
-  Pathankot
-).each do |district_name|
-  district = District.create(
-    name: district_name
+  admin = User.create(
+    email: "admin@example.com",
+    password: "password"
   )
 
-  3.times.each do
-    facility_type = %w(DH SDH CHC PHC).sample
-
-    facility = district.facilities.create(
-      user: admin,
-      name: "#{facility_type} #{Faker::Address.city}"
+  %w(
+    Bathinda
+    Gurdaspur
+    Hoshiarpur
+    Mansa
+    Pathankot
+  ).each do |district_name|
+    district = District.create(
+      name: district_name
     )
 
-    5.times.each do |id|
-      patient = facility.patients.create(
-        user: admin,
-        treatment_number: id + 1,
-        registered_on: Faker::Date.between(3.months.ago, Date.today),
-        name: "Test User #{id + 1}",
-        age: rand(18..80),
-        village: "Test Village #{id + 1}",
-        district: district.name,
-        phone: "9123456789"
+    3.times.each do
+      facility_type = %w(DH SDH CHC PHC).sample
+
+      facility = district.facilities.create(
+        author: admin,
+        name: "#{facility_type} #{Faker::Address.city}"
       )
 
-      3.times.each do
-        measured_on = Faker::Date.between(3.months.ago, Date.today)
-
-        patient.visits.create(
-          user: admin,
-          systolic: rand(110..190),
-          diastolic: rand(70.140),
-          measured_on: measured_on,
-          amlodipine: "10mg",
-          telmisartan: "40mg",
-          next_visit_on: measured_on + 1.month
+      5.times.each do |id|
+        patient = facility.patients.create(
+          author: admin,
+          treatment_number: id + 1,
+          registered_on: Faker::Date.between(3.months.ago, Date.today),
+          name: "Test User #{id + 1}",
+          age: rand(18..80),
+          village: "Test Village #{id + 1}",
+          district: district.name,
+          phone: "9123456789"
         )
+
+        3.times.each do
+          measured_on = Faker::Date.between(3.months.ago, Date.today)
+
+          patient.visits.create(
+            author: admin,
+            systolic: rand(110..190),
+            diastolic: rand(70.140),
+            measured_on: measured_on,
+            amlodipine: "10mg",
+            telmisartan: "40mg",
+            next_visit_on: measured_on + 1.month
+          )
+        end
       end
     end
   end
