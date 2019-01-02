049773d78e4ba207d03d6240994db9833dbdb8df
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-01-02 15:13:50 +0530

    Update SyncMedicalHistoryPayload to have enum strings instead of booleans

--- app/payloads/sync_medical_history_payload.rb
+++ app/payloads/sync_medical_history_payload.rb
@@ -8,19 +8,20 @@ class SyncMedicalHistoryPayload
     @user_id = user_id
   end
 
-  def parse_boolean(value)
-    value || false
+  def parse_boolean_as_enum(value)
+    boolean_value = value || false
+    boolean_value ? 'yes' : 'unknown'
   end
 
   def to_payload
     { id: patient.medical_history_uuid,
       patient_id: patient.patient_uuid,
-      prior_heart_attack: parse_boolean(patient.prior_heart_attack),
-      prior_stroke: parse_boolean(patient.prior_stroke),
-      chronic_kidney_disease: parse_boolean(patient.chronic_kidney_disease),
-      receiving_treatment_for_hypertension: parse_boolean(patient.already_on_treatment),
-      diagnosed_with_hypertension: parse_boolean(patient.diagnosed_with_hypertension),
-      diabetes: parse_boolean(nil), # This information is not present in the red cards
+      prior_heart_attack: parse_boolean_as_enum(patient.prior_heart_attack),
+      prior_stroke: parse_boolean_as_enum(patient.prior_stroke),
+      chronic_kidney_disease: parse_boolean_as_enum(patient.chronic_kidney_disease),
+      receiving_treatment_for_hypertension: parse_boolean_as_enum(patient.already_on_treatment),
+      diagnosed_with_hypertension: parse_boolean_as_enum(patient.diagnosed_with_hypertension),
+      diabetes: parse_boolean_as_enum(nil), # This information is not present in the red cards
       created_at: patient.registered_on_without_timestamp,
       updated_at: Time.now }
   end
