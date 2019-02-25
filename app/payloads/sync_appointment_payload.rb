6dfea170908b468eb6170caa28d929b56c66d624
Timmy Jose <timmy@nilenso.com>
2019-02-25 12:08:44 +0530

    Fixed logic for handling both when the latest visit has or is missing a next visit date

--- app/payloads/sync_appointment_payload.rb
+++ app/payloads/sync_appointment_payload.rb
@@ -9,21 +9,29 @@ class SyncAppointmentPayload
   def to_payload
     visits = patient.visits
     return [] unless visits.present?
-    requests = visits.map { |visit| appointment_payload(visit) }.compact
-    requests.last[:status] = 'scheduled' if requests.present?
-    requests
+
+    visits_by_measurement_date = visits.order(:measured_on)
+    latest_visit = visits_by_measurement_date.last
+    latest_appointment = appointment_payload(latest_visit)
+    appointments = visits_by_measurement_date[0..-2].map { |visit| appointment_payload(visit) }.compact
+
+    if latest_visit.next_visit_on.present?
+      latest_appointment[:status] = 'scheduled'
+      appointments << latest_appointment
+    end
+
+    appointments
   end
 
   private
 
   def appointment_payload(visit)
-    return nil if visit.next_visit_on.blank?
-    { id: visit.appointment_uuid,
-      patient_id: visit.patient.patient_uuid,
-      facility_id: visit.facility.simple_uuid,
-      scheduled_date: visit.next_visit_on,
-      status: 'visited',
-      created_at: visit.measured_on_without_timestamp,
-      updated_at: visit.measured_on_without_timestamp,}
+    {id: visit.appointment_uuid,
+     patient_id: visit.patient.patient_uuid,
+     facility_id: visit.facility.simple_uuid,
+     scheduled_date: visit.next_visit_on,
+     status: 'visited',
+     created_at: visit.measured_on_without_timestamp,
+     updated_at: visit.measured_on_without_timestamp, }
   end
 end
