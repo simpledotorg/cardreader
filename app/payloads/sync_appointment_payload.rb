b69bb5a37d3f3c4f4aed54143665e536d485ea8f
Timmy Jose <timmy@nilenso.com>
2019-02-25 16:56:48 +0530

    Updated code to handle edge case (nil values for bp-only records)

--- app/payloads/sync_appointment_payload.rb
+++ app/payloads/sync_appointment_payload.rb
@@ -11,11 +11,11 @@ class SyncAppointmentPayload
     return [] unless visits.present?
 
     visits_by_measurement_date = visits.order(:measured_on)
-    latest_visit = visits_by_measurement_date.last
-    latest_appointment = appointment_payload(latest_visit)
     appointments = visits_by_measurement_date[0..-2].map { |visit| appointment_payload(visit) }.compact
 
+    latest_visit = visits_by_measurement_date.last
     if latest_visit.next_visit_on.present?
+      latest_appointment = appointment_payload(latest_visit)
       latest_appointment[:status] = 'scheduled'
       appointments << latest_appointment
     end
@@ -26,6 +26,8 @@ class SyncAppointmentPayload
   private
 
   def appointment_payload(visit)
+    return nil if visit.next_visit_on.blank?
+
     {id: visit.appointment_uuid,
      patient_id: visit.patient.patient_uuid,
      facility_id: visit.facility.simple_uuid,
